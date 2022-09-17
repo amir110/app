d3f1c64002a9587eb246d2624c8cd73fee04e100
amir110 <amir.alilou@gmail.com>
2022-09-17 13:50:55 +0430

    init

---
+++ adder/adder.h
@@ -0,0 +1,6 @@
+#ifndef adder_h
+#define adder_h
+
+float add(float a, float b);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+#include "adder/adder.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout<< "hello world" << "sum of 2.4 and 5.6 is : " << add(2.4, 5.6) << "\n";
+    std::getchar();
+    return 0;
+}
